a45f9b123d55c365b3be5ccc9a1b00bb0b7cfbd4
vjones906 <vanessamaikojones@gmail.com>
2020-02-26 12:06:58 -0800

    added horizontal boundaries

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    const float hozBoundary = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 3;
+        float result = 1.0f / num1;
+        Debug.Log(result);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player can't move off the grass to the right
+        if(transform.position.x > hozBoundary)
+        {
+            transform.position = new Vector3(hozBoundary, transform.position.y, transform.position.z);
+        }
+        // Player can't move off the grass to the left
+        if (transform.position.x < -hozBoundary)
+        {
+            transform.position = new Vector3(-hozBoundary, transform.position.y, transform.position.z);
+        }
+        float hozInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hozInput * Time.deltaTime * speed);
+    }
+}
