4b88a960ff144c56b4f9d020e9328e3fe2f5e194
CBTE Account <cbte@ac.sdmesa.net>
2020-03-04 11:40:20 -0800

    added spawn manager

---
+++ Assets/Scripts/DiscoLighting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoLighting : MonoBehaviour
+{
+    public Light discoLight;
+    public Color discoLightColor;
+    private Color[] colors = new Color[6];
+    // Start is called before the first frame update
+    void Start()
+    {
+        discoLightColor = Color.magenta;
+
+        for (int i = 0; i < 6; i++)
+        {
+            colors[i] = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            Debug.Log(colors[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % 20 == 0)
+        {
+            discoLight.enabled = false;
+        }
+        else
+        {
+            discoLight.enabled = true;
+        }
+        discoLight.color = colors[Time.frameCount % 6];
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animals;
+    private const float xRange = 10;
+    private const float spawnPoint2 = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            int randomAnimals = Random.Range(0, animals.Length);
+            Instantiate(animals[randomAnimals], new Vector3(Random.Range(-10.0f, 10.0f), 0, spawnPoint2), animals[randomAnimals].transform.rotation);
+        }
+    }
+}
