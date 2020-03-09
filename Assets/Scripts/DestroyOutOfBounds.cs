f2e54a7d89bfede5f1dd08d2bf0e7781fbc3a13f
CBTE Account <cbte@ac.sdmesa.net>
2020-03-09 12:00:32 -0700

    added collision effects

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -22,6 +22,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z <= outofBoundsSouth)
         {
+            Debug.Log("Game Over");
             Destroy(gameObject);
         }
     }
